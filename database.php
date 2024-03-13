896948539e52bdd8ddbb1e36deec3049a45580be
huwancarlows <murillojuancarlos499@gmail..com>
2024-03-13 22:23:03 +0800

    ez

--- database.php
+++ database.php
@@ -4,21 +4,17 @@ class Database
     private $host = "localhost";
     private $username = "root";
     private $password = "";
-    private $database = "api_db";
+    private $database = "apidb";
     public $conn;
 
     public function __construct()
     {
         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
 
+        // Check connection
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
-    }
-
-    public function getConnection()
-    {
-        return $this->conn;
     }
 }
 ?>
